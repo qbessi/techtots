394ba602e00a8fd8e54f8e9c38899756c83a830d
MK <github@bessi.io>
2025-06-01 19:11:09 +0100

    stripe payment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: 'https://techtots-five.vercel.app',
-  output: 'static',
-  integrations: [tailwind(), mdx(), sitemap(), icon()]
+  output: 'server',
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  adapter: vercel()
 });
