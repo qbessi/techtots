18bc6d80f9187648d90e7b99e50b494b951d3156
MK <github@bessi.io>
2025-06-01 17:57:05 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from "astro/config";
-import staticAdapter from "@astrojs/adapter-static";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  site: "https://techtots-five.vercel.app/", // for sitemap
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
-  adapter: staticAdapter(),
+  site: 'https://techtots-five.vercel.app',
+  output: 'static',
+  integrations: [tailwind(), mdx(), sitemap(), icon()]
 });
