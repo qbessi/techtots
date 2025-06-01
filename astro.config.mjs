337f09ff0029979c1da7dafd28e5f4b1e44b9196
MK <github@bessi.io>
2025-06-01 19:19:22 +0100

    stripe

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: 'https://techtots-five.vercel.app',
-  output: 'server',
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
-  adapter: vercel()
+  output: 'static',
+  integrations: [tailwind(), mdx(), sitemap(), icon()]
 });
